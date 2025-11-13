0194d8419646e83941f16d45845485f864a95895
Alexandr Ivanov <stuardryan14529@gmail.com>
2025-11-13 22:19:11 +0300

    Описаны сериализаторы и вью для моделей NetworkLink и Product, настроена админ-панель

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from network.models import NetworkLink, Product
+
+
+@admin.action(description="Очистить задолженность")
+def clean_arrears(modeladmin, request, queryset):
+    """Очищает задолженность перед поставщиком у выбранных объектов"""
+    queryset.update(arrears=0)
+
+
+@admin.register(NetworkLink)
+class NetworkLinkAdmin(admin.ModelAdmin):
+    """Фильтрует по названию города"""
+    list_display = ("name", "email", "city", "arrears", "hierarchy_level")
+    list_filter = ("city",)
+    actions = [clean_arrears]
+    readonly_fields = ("level", "created_at")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Выводит информацию по поставщику"""
+    list_display = ("name", "model", "release_date", "supplier")
+    list_filter = ("release_date",)
--- network/views.py
+++ network/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from network.models import NetworkLink, Product
+from network.serializers import NetworkLinkSerializer, ProductSerializer
+
+
+class IsActiveEmployee(permissions.BasePermission):
+    """Предоставляет доступ только активным сотрудникам"""
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_active
+
+class NetworkLinkViewSet(viewsets. ModelViewSet):
+    """CRUD для модели NetworkLink с возможностью фильтрации по стране"""
+    queryset = NetworkLink.objects.all()
+    serializer_class = NetworkLinkSerializer
+    permission_classes = [IsActiveEmployee]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["country"]
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """CRUD для модели Product с возможностью фильтрации по стране"""
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsActiveEmployee]
+    
+
